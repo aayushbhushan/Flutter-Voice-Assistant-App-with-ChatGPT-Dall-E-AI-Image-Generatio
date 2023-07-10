547af39378d87550fd0bd92e6b3f62d62a44ff07
Aayush Bhushan <aayushbhushan2001@gmail.com>
2023-07-11 00:25:40 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/aayush/allen/allen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aayush.allen.allen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openAIAPIKey = 'sk-30Y2wZz3Bq6Z0VlQ0jwtT3BlbkFJRhOwImud8C7KbSg0eRu9j';
